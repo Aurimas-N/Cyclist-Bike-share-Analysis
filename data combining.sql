6fe9779eeed97da402ba586d86acbccb068d1a6a
AurimasN <Naujalisaurimas@gmail.com>
2025-02-17 17:55:23 +0200

    combining.sql

--- data combining.sql
+++ data combining.sql
@@ -1,1 +1,27 @@
-## combining data
+-- Combining data from all 12 tables.
+
+CREATE TABLE `bikes-451213.bikesall.cyclistic_data` AS (
+  SELECT * FROM `bikes-451213.bikesall.bike1` 
+  UNION ALL
+  SELECT * FROM `bikes-451213.bikesall.bike10` 
+  UNION ALL
+  SELECT * FROM `bikes-451213.bikesall.bike11` 
+  UNION ALL
+  SELECT * FROM `bikes-451213.bikesall.bike12` 
+  UNION ALL
+  SELECT * FROM `bikes-451213.bikesall.bike2` 
+  UNION ALL
+  SELECT * FROM `bikes-451213.bikesall.bike3` 
+  UNION ALL
+  SELECT * FROM `bikes-451213.bikesall.bike4` 
+  UNION ALL
+  SELECT * FROM `bikes-451213.bikesall.bike5` 
+  UNION ALL
+  SELECT * FROM `bikes-451213.bikesall.bike6` 
+  UNION ALL
+  SELECT * FROM `bikes-451213.bikesall.bike7` 
+  UNION ALL
+  SELECT * FROM `bikes-451213.bikesall.bike8` 
+  UNION ALL
+  SELECT * FROM `bikes-451213.bikesall.bike9`
+  );
