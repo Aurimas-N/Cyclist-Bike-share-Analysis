02975cecb98bf3eccaca015dedad19b1d1c1ed78
AurimasN <Naujalisaurimas@gmail.com>
2025-02-18 16:07:01 +0200

    Update data_exploration.sql

--- data_exploration.sql
+++ data_exploration.sql
@@ -6,6 +6,7 @@ FROM `bikes-451213.bikesall.INFORMATION_SCHEMA.COLUMNS`
 WHERE table_name = 'cyclistic_data';
 
 -- Identifying the nulls in columns.
+
 SELECT COUNT(*) - COUNT(ride_id) ride_id,
  COUNT(*) - COUNT(rideable_type) rideable_type,
  COUNT(*) - COUNT(started_at) started_at,
@@ -21,4 +22,42 @@ SELECT COUNT(*) - COUNT(ride_id) ride_id,
  COUNT(*) - COUNT(member_casual) member_casual
 bikes-451213.bikesall.cyclistic_data
 
--- 
+-- Checking for dublicates, rider_id is our primary key, that means, it cant have any dublicates, and those will be removed.
+  
+SELECT ride_id, COUNT(*) AS duplicate_count
+FROM `bikes-451213.bikesall.cyclistic_data`
+GROUP BY ride_id
+HAVING COUNT(*) > 1;
+
+-- Checking for inconsistent Categorical values for bike types.
+
+SELECT DISTINCT rideable_type
+FROM `bikes-451213.bikesall.cyclistic_data`;
+
+-- Inconsistent Categorical values for membership
+
+SELECT DISTINCT member_casual
+FROM `bikes-451213.bikesall.cyclistic_data`;
+
+-- Checking for ilogical start and end time dates, those has to be removed.
+
+SELECT * 
+FROM `bikes-451213.bikesall.cyclistic_data`
+WHERE ended_at < started_at;
+
+-- Checking for extreme outliers.
+
+SELECT 
+  ride_id, 
+  started_at, 
+  ended_at, 
+  TIMESTAMP_DIFF(ended_at, started_at, MINUTE) AS ride_duration_minutes
+FROM `bikes-451213.bikesall.cyclistic_data`
+ORDER BY ride_duration_minutes DESC
+LIMIT 10;
+
+-- Checking if ride_id lenght is correct in all rows.
+
+SELECT LENGTH(ride_id) AS length_ride_id, COUNT(ride_id) AS nmbr_rows
+FROM bikes-451213.bikesall.cyclistic_data
+GROUP BY length_ride_id;
