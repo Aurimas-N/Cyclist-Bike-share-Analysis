4cbbd89066b61295bef09407de9d58886e90616a
AurimasN <Naujalisaurimas@gmail.com>
2025-02-18 16:32:13 +0200

    Update data_cleaning.sql

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,7 +1,1 @@
--- rider_id is our primary key, that means, it cant have any dublicates.
-SELECT ride_id, COUNT(*) AS duplicate_count
-FROM `bikes-451213.bikesall.cyclistic_data`
-GROUP BY ride_id
-HAVING COUNT(*) > 1;
--- I found out that there is 211 dublicates, and removed them with code below.
-
+--
