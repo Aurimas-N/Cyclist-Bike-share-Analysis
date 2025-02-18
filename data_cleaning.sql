d38103a80339181d0be1295e9efe7d2a81c95183
AurimasN <Naujalisaurimas@gmail.com>
2025-02-18 19:47:20 +0200

    Update data_cleaning.sql

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,1 +1,15 @@
+-- Deleted rows with ride_id dublicates, since its primary key, they have to be unique.
+
+DELETE FROM `bikes-451213.bikesall.cyclistic_data`
+WHERE ride_id IN (
+  SELECT ride_id
+  FROM (
+    SELECT
+      ride_id,
+      ROW_NUMBER() OVER (PARTITION BY ride_id ORDER BY ride_id) AS row_num
+    FROM `bikes-451213.bikesall.cyclistic_data`
+  )
+  WHERE row_num > 1
+);
+
 --
