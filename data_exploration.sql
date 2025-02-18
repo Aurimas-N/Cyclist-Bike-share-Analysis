1549d982a020d9a24a80de1f06d7aafcab775701
AurimasN <Naujalisaurimas@gmail.com>
2025-02-18 15:42:27 +0200

    Update data_exploration.sql

--- data_exploration.sql
+++ data_exploration.sql
@@ -1,1 +1,24 @@
---
+-- Verifying if columns have correct data types.
+
+SELECT 
+  column_name, data_type 
+FROM `bikes-451213.bikesall.INFORMATION_SCHEMA.COLUMNS`
+WHERE table_name = 'cyclistic_data';
+
+-- Identifying the nulls in columns.
+SELECT COUNT(*) - COUNT(ride_id) ride_id,
+ COUNT(*) - COUNT(rideable_type) rideable_type,
+ COUNT(*) - COUNT(started_at) started_at,
+ COUNT(*) - COUNT(ended_at) ended_at,
+ COUNT(*) - COUNT(start_station_name) start_station_name,
+ COUNT(*) - COUNT(start_station_id) start_station_id,
+ COUNT(*) - COUNT(end_station_name) end_station_name,
+ COUNT(*) - COUNT(end_station_id) end_station_id,
+ COUNT(*) - COUNT(start_lat) start_lat,
+ COUNT(*) - COUNT(start_lng) start_lng,
+ COUNT(*) - COUNT(end_lat) end_lat,
+ COUNT(*) - COUNT(end_lng) end_lng,
+ COUNT(*) - COUNT(member_casual) member_casual
+bikes-451213.bikesall.cyclistic_data
+
+-- 
